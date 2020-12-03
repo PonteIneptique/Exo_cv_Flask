c6681c528094409c74f3b0c4c42542291f92d05d
victor <victor.meynaud@chartes.psl.eu>
2020-12-03 10:21:03 +0100

    Commit général de app.py + les templates Flask

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template
+
+#Nom de l'application (obligatoire)
+app = Flask(__name__)
+
+#Données utilisées par l'application. Syntaxe permettant de rendre la variable 'data' accessible partout avec Jinja (sinon on a un 'data not defined'
+app.jinja_env.globals['data'] = [
+    {
+        "diplome": "bac ES",
+        "annee": "2014",
+        "lieu": "Antony",
+    },
+    {
+        "diplome": "licence histoire",
+        "annee": "2017",
+        "lieu": "Sorbonne Université",
+    },
+    {
+        "diplome": "licence anglais",
+        "annee": "2017",
+        "lieu": "Sorbonne Université",
+    },
+    {
+        "diplome": "master histoire",
+        "annee": "2019",
+        "lieu": "Sorbonne Université",
+    },
+    {
+        "diplome": "master TNAH",
+        "annee": "2021",
+        "lieu": "ENC",
+    },
+    {
+        "emploi": "stage archiviste",
+        "annee": "2020",
+        "lieu": "Groupe Crédit Agricole",
+    },
+    {
+        "emploi": "employé de banque",
+        "annee": "2019",
+        "lieu": "BNP",
+    },
+{
+        "emploi": "employé de restauration",
+        "annee": "2016",
+        "lieu": "Centre sportif de Bougival",
+    },
+]
+#Chemin vers la page d'accueil
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#Chemin vers la page 'variable' qui change selon les boutons cliqués. Le chemin sera sous la forme variable/nombre.
+@app.route("/variable/<int:place_id>")
+def variable(place_id):
+    return render_template("variable.html", place_id=place_id)
+
+if __name__ == "__main__":
+#Permet d'utiliser le débugger Jinja
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Ce dossier est un exercice consistant à créer son CV avec Flask (M2 TNAH 2020).
